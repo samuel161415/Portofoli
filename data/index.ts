2967ec8dd9a4ac5c21c6c9612fd24b95add9b996
samuel161415 <samuelnegalign1@gmail.com>
2024-08-28 21:44:59 +0300

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,250 @@
+export const navItems = [
+  
+  { name: "Projects", link: "#projects" },
+  { name: "Skills", link: "#skills" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Africa To Silicon Valley ",
+    des: "Welcome to a2sv.org, the digital heart of a non-profit committed to providing free education services to African students. Our site showcases a transformative journey, highlighting milestones, impact stories, and our vision for the future.",
+    img: "/a2sv_web.png",
+    iconLists: ["/nuxt.svg", "/tail.svg", "/vuetify.svg", "/node.svg", "/cloud.svg"],
+    link: "https://a2sv.org/",
+  },
+  {
+    id: 2,
+    title: "A2SV Hackathon",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/hackathon_web.png",
+    iconLists: ["/angular.svg", "/tail.svg", "/ts.svg", "/cloud.svg"],
+    link: "https://hackathon.a2sv.org/",
+  },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+  //   link: "https://venturi-frontend.vercel.app/",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "/ui.apple.com",
+  // },
+];
+
+export const skills = [
+  {
+    name: "React/Next.js",
+    description: `Hello, my name is Samuel Negalign. With about 4 years of experience in React and Next.js, I specialize in making websites more mobile-friendly and visually appealing. I excel in state management using hooks, context API, and Redux, and leverage testing tools like Jest to ensure high-quality code. My use of Tailwind CSS, Styled Components, and Material UI allows me to craft user-friendly interfaces that are both scalable and maintainable.`,
+  },
+  {
+    name: "Vue/Nuxt.js",
+    description: `Over the last three years, I have honed my skills in Vue.js, particularly with Nuxt.js on projects like A2sv.org. My role involved overseeing development and integrating technologies like Vuetify and Tailwind CSS, enhancing my flexibility and efficiency in project completion. This background has equipped me to handle various challenges and deliver sleek, functional websites.`,
+  },
+  {
+    name: "Angular",
+    description: `In my past 2 years working with Angular, I've developed robust websites and specialized in using RxJS for state management. My experience has provided me with a deep understanding of Angular's architecture, enabling me to build well-structured and performant applications that meet diverse client needs.`,
+  },
+  {
+    name: "Node.js/Express.js",
+    description: `With nearly 4 years of experience in backend development, I've extensively used Node.js and Express.js to create scalable and efficient server-side applications. My preference for Node.js stems from its open-source nature and compatibility with the JavaScript ecosystem, which facilitates seamless integration with various frontend frameworks. My backend skill set also includes working with MongoDB and occasionally Firebase, providing comprehensive database solutions.`,
+  },
+  {
+    name: "DevOps Skills",
+    description: `My DevOps expertise includes proficiency in Git for version control and a strong grasp of CI/CD pipelines using tools like GitHub Actions. This skill set ensures efficient workflow management, from code integration and testing to deployment, aligning well with any project's requirements for a smooth and reliable development process.`,
+  },
+  {
+    name: "DSA Skills",
+    description: `Solving over 400 competitive programming problems on platforms like LeetCode and Codeforces has sharpened my problem-solving skills. This expertise is crucial in developing efficient websites, handling complex states, and utilizing data structures effectively. My main languages for these challenges are Python, Java, and JavaScript.`,
+  }
+];
+
+
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "cloud run ",
+    img: "/cloud-run.png"
+    // nameImg: "/a2sv.svg",
+  },
+  // {
+  //   id: 3,
+  //   name: "HOSTINGER",
+  //   img: "/host.svg",
+  //   nameImg: "/hostName.svg",
+  // },
+  // {
+  //   id: 4,
+  //   name: "stream",
+  //   img: "/s.svg",
+  //   nameImg: "/streamName.svg",
+  // },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Developer - A2SV",
+    desc: "Developed and maintained website tools using React/Next.js, Vue/Nuxt.js, and Angular, solving complex problems and collaborating with cross-functional teams to enhance system performance.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full stack developer - Bank of Abyssinia",
+    desc: "Developed and optimized full-stack web applications using Node.js, React.js, and MongoDB, collaborating with clients to translate requirements into functional solutions, and managed IT infrastructures.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Successfully developed three freelance projects using Nuxt.js/Vue.js, React/Next.js, and Node.js/Express, delivering robust and efficient solutions tailored to client specifications.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed high-performance, full-stack web applications using Node.js, React.js, and MongoDB, collaborated with clients to meet their needs, and conducted testing and optimization for enhanced usability.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link : "https://github.com/samuel161415"
+  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  //   link : ""
+  // },
+  {
+    id: 3,
+    img: "/link.svg",
+    link : "https://www.linkedin.com/in/samuel-negalign/"
+  },
+];
